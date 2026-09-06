882d3c1e3501a3ac4b830be81204c630857a000b
jackq120219 <166075321+jackq120219@users.noreply.github.com>
2026-09-05 19:08:44 -0500

    Configure Queenan Capital ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'off',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
+];
+
+export default eslintConfig;
