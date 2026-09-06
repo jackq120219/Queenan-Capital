717e46b2815812e8dc9a5fd4f23219fb920163a3
jackq120219 <166075321+jackq120219@users.noreply.github.com>
2026-09-06 11:09:58 -0500

    Point Queenan robots sitemap to canonical www host

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://queenancapital.com/sitemap.xml',
+    sitemap: 'https://www.queenancapital.com/sitemap.xml',
   };
 }
